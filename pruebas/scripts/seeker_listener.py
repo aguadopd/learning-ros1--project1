7fc059d916e37db9d65da3d3f5e7ec65e3075094
Ednip7 <ednip7@gmail.com>
2021-08-11 21:40:55 -0300

    Trying to subscribe to a topic.

--- pruebas/scripts/seeker_listener.py
+++ pruebas/scripts/seeker_listener.py
@@ -1,13 +1,25 @@
 #!/usr/bin/env python
 import rospy
-from std_msgs.msg import String
-from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 from darknet_ros_msgs.msg import BoundingBoxes
+from std_msgs.msg import Header
+from std_msgs.msg import String
 
 def callback(data):
-    rospy.loginfo
+    rospy.loginfo(rospy.get_caller_id() + 'Heard %s', data.data)
 
+def callback(data):
+    for box in data.bounding_boxes:
+        rospy.loginfo(
+            "Xmin: {}, Xmax: {}, Ymin:{}, Ymax: {}".format(
+                box.xmin, box.xmax, box.ymin, box.ymax
+            )
+        )
 def seeker_listener():
-    rospy.init_node('seeker_listener', anonymous = True)
-    rospy.Subscriber("")
+    rospy.init_node('seeker_listener', anonymous=True)
+    rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, callback)
+    rospy.spin()
+    
+def main():
+    while not rospy.is_shutdown():
+
