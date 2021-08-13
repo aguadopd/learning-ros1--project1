931db00a214a33211f12f4da1c908bb6f1fa2c8e
Ednip7 <ednip7@gmail.com>
2021-08-13 10:47:44 -0300

    Suggestions added to package.xml and seeker_listenerner.py Script docstring still pending

--- pruebas/scripts/seeker_listener.py
+++ pruebas/scripts/seeker_listener.py
@@ -6,20 +6,27 @@ from std_msgs.msg import Header
 from std_msgs.msg import String
 
 def callback(data):
-    for box in data.bounding_boxes:
-        rospy.loginfo(
-            "I found: {} ".format(box.Class)
-        )
+    rospy.loginfo((f'I found :{", ".join([box.Class for box in data.bounding_boxes])}'))
+#def callback(data):
+#    for box in data.bounding_boxes:
+#        rospy.loginfo(
+#            "I found: {} ".format(box.Class)
+#        )
 
 def seeker_listener():
     rospy.init_node('seeker_listener', anonymous=True)
     rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, callback)
     rospy.spin()
-    
+
+
 if __name__ == '__main__':
+    rospy.loginfo("Starting seeker_listener.")  # Not printed in the console... why???
+    print("Waiting for bounding_boxes messages to be published...")
     try:
         while not rospy.is_shutdown():
             seeker_listener()
     except rospy.ROSInterruptException:
         pass
+    finally:
+        rospy.loginfo('Closing...')
 
